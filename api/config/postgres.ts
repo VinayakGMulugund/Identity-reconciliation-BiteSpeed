5a67473e3f72410ea33c498acdbee8920b111748
Vinayak Mulugund <mulgundvinay@gmail.com>
2024-06-21 00:31:01 +0530

    Dialect module for deployment

--- api/config/postgres.ts
+++ api/config/postgres.ts
@@ -1,3 +1,4 @@
+import pg from 'pg'
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv'
 
@@ -7,7 +8,8 @@ const sequel = new Sequelize(
   process.env.DB_URL!,
   {
     username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    dialectModule: pg
   }
 );
 
--- dist/config/postgres.js
+++ dist/config/postgres.js
@@ -4,12 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sequel = void 0;
+const pg_1 = __importDefault(require("pg"));
 const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const sequel = new sequelize_1.Sequelize(process.env.DB_URL, {
     username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    dialectModule: pg_1.default
 });
 exports.sequel = sequel;
 //# sourceMappingURL=postgres.js.map
